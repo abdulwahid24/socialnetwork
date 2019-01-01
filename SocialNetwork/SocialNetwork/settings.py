64152f7786c86bf0d0ba53b4db8539abad1ce1a8
Abdul Wahid <abdulwahid24@gmail.com>
2019-01-02 02:09:36 +0530

    Configured the docker compose with postgres, hstore and django project.

--- SocialNetwork/SocialNetwork/settings.py
+++ SocialNetwork/SocialNetwork/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'SocialNetwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'socialNetworkDB',
+        'HOST': 'db',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'SocialNetwork123#'
     }
 }
 
